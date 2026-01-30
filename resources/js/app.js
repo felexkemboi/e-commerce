01e2265bc8164557e55a0e9d8393604bd1cc5a3c
felexkemboi <felokemboi10@gmail.com>
2026-01-30 17:48:32 +0300

    Remove unsued router in app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,7 +5,6 @@ import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
-import router from './routes'; 
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
@@ -19,7 +18,6 @@ createInertiaApp({
         return createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue)
-            .use(router)
             .mount(el);
     },
     progress: {
