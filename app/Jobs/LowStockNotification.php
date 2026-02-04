0ff964c653f393329ddab3cd57a90fc68ce83909
felexkemboi <felokemboi10@gmail.com>
2026-02-04 21:18:32 +0300

    Add the job to be executed

--- app/Jobs/LowStockNotification.php
+++ app/Jobs/LowStockNotification.php
@@ -2,26 +2,30 @@
 
 namespace App\Jobs;
 
-use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Foundation\Queue\Queueable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\LowStockMail;
+use App\Models\Product;
 
 class LowStockNotification implements ShouldQueue
 {
+    use Dispatchable;
+    use InteractsWithQueue;
     use Queueable;
+    use SerializesModels;
 
-    /**
-     * Create a new job instance.
-     */
-    public function __construct()
+    public function __construct(public Product $product)
     {
-        //
     }
 
-    /**
-     * Execute the job.
-     */
-    public function handle(): void
+    public function handle()
     {
-        //
+        $emails = explode(',', config('mail.low_stock_emails'));
+
+        Mail::to($emails)->send(new LowStockMail($this->product));
     }
 }
