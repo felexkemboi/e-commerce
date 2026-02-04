43e619f43cd05743aad2290a699db21a1ad522ed
felexkemboi <felokemboi10@gmail.com>
2026-02-04 20:36:13 +0300

    Update config files

--- config/mail.php
+++ config/mail.php
@@ -111,8 +111,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'admin@ecommerce.com'),
+        'name' => env('MAIL_FROM_NAME', 'Ecommerce Platform'),
     ],
 
     'low_stock_emails' => env('LOW_STOCK_EMAILS'),
