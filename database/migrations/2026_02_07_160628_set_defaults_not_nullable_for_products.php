bc20627fe642d9a96efbf17281d2bfb5bb8de3d7
felexkemboi <felokemboi10@gmail.com>
2026-02-07 19:07:58 +0300

    make non-nullable columns have default value

---
+++ database/migrations/2026_02_07_160628_set_defaults_not_nullable_for_products.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->string('name')->default('Product')->nullable(false)->change();
+            $table->integer('price')->default(0)->nullable(false)->change();
+            $table->integer('stock_quantity')->default(0)->nullable(false)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
